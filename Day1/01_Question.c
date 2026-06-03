b673c13ac0b345ee26a561e504d4318206efe35d
Rupal Gupta <rupal30gupta@gmail.com>
2026-06-03 23:59:07 +0530

    Day1

---
+++ Day1/01_Question.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int n;
+long long factorial=1;
+printf("enter a number");
+scanf("%d",&n);
+for( int i=1;i<=n;i++){
+factorial*=i;
+} 
+printf("factorial of %d =%lld\n",n,factorial);
+return 0;
+}
---
+++ Day1/04_Question.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0;
+printf("enter a number ");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+sum += i;
+}
+printf("sum of first %d natural numbers =%d",n,sum);
+return 0;
+}
