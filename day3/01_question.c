ae7946e59995d50e6e4c8d6ffc71ad1803ac090c
Rupal Gupta <rupal30gupta@gmail.com>
2026-06-04 11:19:55 +0530

    day 3 uploaded

---
+++ day3/01_question.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int num, i, prime = 1;
+ printf("Enter a number: ");
+ scanf("%d", &num);
+ if (num <= 1) {
+ prime = 0;
+ } else {
+  for (i = 2; i < num; i++) {
+ if (num % i == 0) {
+  prime = 0;
+ break;
+  }
+  }
+  }
+ if (prime)
+ printf("%d is a Prime Number.\n", num);
+ else
+ printf("%d is not a Prime Number.\n", num);
+ return 0;
+}
